30682cce6100253fc67bc7103307cddce8508a31
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-03-21 14:54:34 -0300

    Atualizar com novas funções o script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function adicionarFilme () {
-    console.log("clicou")
+    var filmeFavorito = document.getElementById("filme").value;
+    if (filmeFavorito.endsWith(".JPG")){
+     listarFilmesNaTela()
+    } else {
+        console.error("Endereço de filme inválido");
+    };
+    document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela () {
+    var elementoFilmeFavorito = "<img src=" + filmeFavorito + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFilmeFavorito;
 }
